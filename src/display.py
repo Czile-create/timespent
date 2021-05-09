289e503a8ea190b3a832b2aab2e01915c6953bfb
Czile <Czile@foxmail.com>
2021-05-09 21:08:33 +0800

    增加了条形图显示

--- src/display.py
+++ src/display.py
@@ -11,6 +11,12 @@ def getweektime():
         ans.append(int(time.mktime((today - datetime.timedelta(days=i)).timetuple())))
     return ans
 
+def showchart(i, j):
+    maxsize = os.get_terminal_size().columns - 6
+    numofequals = int(j * maxsize / i)
+    print(" ( {}{} ) ".format('='*numofequals, ' '*(maxsize - numofequals)))
+    
+
 def readfile(filename):
     t = []
     if os.path.isfile(filename):
@@ -23,11 +29,13 @@ def display(s, t):
     totaltime = 0
     for i in t:
         totaltime += int(i[1])
-    print("You use the computer {} {}.\n".format(datetime.timedelta(seconds=totaltime * 10), s))
+    print("\033[1;30;42m You use the computer {} {}.\033[0m\n".format(datetime.timedelta(seconds=totaltime * 10), s))
     t.sort(key=lambda x: int(x[1]), reverse=True)
     print("Here are the Apps you spent most of the time {}:".format(s))
-    for i in t:
-        print("{}, {}".format(i[0], datetime.timedelta(seconds=int(i[1]) * 10)))
+    maxsize = max(len(i[0]) for i in t)
+    for i in t[:min(len(t), 5)]:
+        print("{}{}{}".format(i[0].ljust(maxsize), " "*(os.get_terminal_size().columns - 8 - maxsize), str(datetime.timedelta(seconds=int(i[1]) * 10)).rjust(8)))
+        showchart(int(t[0][1]), int(i[1]))
 
 
 def today(dirt):
