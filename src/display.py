582cb8c184ebcfe6c84b3111d023f6b53f19d45d
Czile <Czile@foxmail.com>
2021-05-10 09:26:34 +0800

    更新了显示效果

--- src/display.py
+++ src/display.py
@@ -11,11 +11,13 @@ def getweektime():
         ans.append(int(time.mktime((today - datetime.timedelta(days=i)).timetuple())))
     return ans
 
-def showchart(i, j):
-    maxsize = os.get_terminal_size().columns - 6
+def showchart(i, j, maxsize):
+    maxsize = maxsize - 6
     numofequals = int(j * maxsize / i)
-    print(" ( {}{} ) ".format('='*numofequals, ' '*(maxsize - numofequals)))
-    
+    return (" ( {}{} ) ".format('='*numofequals, ' '*(maxsize - numofequals)))
+
+# TODO:
+# def showgeneral(i, j, )
 
 def readfile(filename):
     t = []
@@ -26,16 +28,18 @@ def readfile(filename):
     return [(i.split(', ')) for i in t]
 
 def display(s, t):
+    # TODO:
+    # showgeneral()
     totaltime = 0
     for i in t:
         totaltime += int(i[1])
-    print("\033[1;30;42m You use the computer {} {}.\033[0m\n".format(datetime.timedelta(seconds=totaltime * 10), s))
+    print("{}{}{}{}".format(s.ljust(10), ' '*(os.get_terminal_size().columns - 10 - 16), "{}%".format(int(totaltime * 1000 / 3600 / 24)).ljust(7), str(datetime.timedelta(seconds=totaltime * 10)).rjust(9)))
     t.sort(key=lambda x: int(x[1]), reverse=True)
     print("Here are the Apps you spent most of the time {}:".format(s))
-    maxsize = max(len(i[0]) for i in t)
+    maxsize = max(len(i[0]) for i in t[:min(len(t), 5)])
     for i in t[:min(len(t), 5)]:
-        print("{}{}{}".format(i[0].ljust(maxsize), " "*(os.get_terminal_size().columns - 8 - maxsize), str(datetime.timedelta(seconds=int(i[1]) * 10)).rjust(8)))
-        showchart(int(t[0][1]), int(i[1]))
+        print("{}{}{}{}".format(i[0].ljust(maxsize), showchart(int(t[0][1]), int(i[1]), os.get_terminal_size().columns - 16 - maxsize), "{}%".format(int(int(i[1]) * 1000 / 3600 / 24)).ljust(7), str(datetime.timedelta(seconds=int(i[1]) * 10)).rjust(9)))
+        # showchart(int(t[0][1]), int(i[1]))
 
 
 def today(dirt):
