04039722878189bbb32d5f4bf7b1b4fdea2cd377
Czile <Czile@foxmail.com>
2021-05-11 14:53:43 +0800

    Add Remarks

--- src/display.py
+++ src/display.py
@@ -1,9 +1,11 @@
 import os
 import time, datetime
 
+# 获取当天的时间戳
 def getname():
     return int(time.mktime(datetime.date.today().timetuple()))
 
+# 获取一周的时间戳
 def getweektime():
     ans = []
     today = datetime.date.today()
@@ -11,24 +13,33 @@ def getweektime():
         ans.append(int(time.mktime((today - datetime.timedelta(days=i)).timetuple())))
     return ans
 
+# 返回条形图
+# i: 总数
+# j: 当前类别数
+# maxsize: 条形图的总大小
+# TODO: 报错处理：maxsize不够
 def showchart(i, j, maxsize):
     maxsize = maxsize - 6
     numofequals = int(j * maxsize / i)
     return (" ( {}{} ) ".format('='*numofequals, ' '*(maxsize - numofequals)))
 
+# 将字体颜色调成蓝色
 def blue(s):
     return "\033[34m{}\033[0m".format(s)
 
+# 将字体颜色调成蓝绿色
 def blue_green(s):
     return "\033[36m{}\033[0m".format(s)
 
+# 将字体颜色调成绿色
 def green(s):
     return "\033[32m{}\033[0m".format(s)
 
+# 将字体颜色调成白色
 def white(s):
     return "\033[37m{}\033[0m".format(s)
 
-# TODO:
+# 显示总览条形图
 def showgeneral(t, totaltime, maxsize):
     maxsize = maxsize - 6
     block = []
@@ -38,7 +49,8 @@ def showgeneral(t, totaltime, maxsize):
         block.append(0)
     return " ( {}{}{}{} ) ".format(block[0] * green('='), block[1] * blue('='), block[2] * blue_green('='), (maxsize - sum(block)) * white('='))
     
-
+# 读入文件数据
+# 文件格式：[[programs name, using times]]
 def readfile(filename):
     t = []
     if os.path.isfile(filename):
@@ -47,32 +59,43 @@ def readfile(filename):
                 t = f.read().splitlines()
     return [(i.split(', ')) for i in t]
 
+# 显示数据
+# s: 提示数据
+# t: 已读入的数据数组
 def display(s, t):
-    # TODO:
-    # showgeneral()
+    # 计算使用的总时间
+    # 总时间 = 总使用次数 * 10秒
     totaltime = 0
     for i in t:
         totaltime += int(i[1])
+    
+    # 按应用使用时间从大到小排序
     t.sort(key=lambda x: int(x[1]), reverse=True)
+
+    # 输出总览图
     print("{}{}{}{}".format(s.ljust(10), showgeneral(t, totaltime, os.get_terminal_size().columns - 10 - 16), "{}%".format(int(totaltime * 1000 / 3600 / 24)).ljust(7), str(datetime.timedelta(seconds=totaltime * 10)).rjust(9)))
+
+    # 输出总览图注释：前三使用时间应用名称
     max3 = []
     for i in t[:min(len(t), 3)]:
         max3.append("={}    ".format(i[0]))
     while (len(max3) < 3):
         max3.append("")
     print("{}{}{}{}".format(green(max3[0]), blue(max3[1]), blue_green(max3[2]), white("=other")))
+
+    # 输出前五个应用具体使用时间
     print("Here are the Apps you spent most of the time {}:".format(s))
     maxsize = max(len(i[0]) for i in t[:min(len(t), 5)])
     for i in t[:min(len(t), 5)]:
         print("{}{}{}{}".format(i[0].ljust(maxsize), showchart(int(t[0][1]), int(i[1]), os.get_terminal_size().columns - 16 - maxsize), "{}%".format(int(int(i[1]) * 1000 / 3600 / 24)).ljust(7), str(datetime.timedelta(seconds=int(i[1]) * 10)).rjust(9)))
-        # showchart(int(t[0][1]), int(i[1]))
 
-
+# 输出今日的图表
 def today(dirt):
     filename = dirt+"/.timespent/{}.csv".format(getname())
     t = readfile(filename)
     display("Today", t)
 
+# 输出这周以来的图表
 def week(dirt):
     filenames = getweektime()
     t = []
@@ -90,7 +113,10 @@ def week(dirt):
     
     display("This week", t)
 
-# print(white("Hello World"))
+# 本程序实际上可以输出任意时间以来的图表
+# TODO: 参数 -d 表示输出多少日以来的图表
+# 本程序可以找任意时间以来的某一应用使用时间
+# TODO: 参数 -p 表示某一应用的使用时间统计
 dirt = os.environ['HOME']
 today(dirt)
 print("\n")
--- src/getWindows.py
+++ src/getWindows.py
@@ -1,39 +1,60 @@
 import os
 import time
 import datetime
+
+# 获取当前的焦点窗口名称
 def getfocus():
     return os.popen("xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_CLASS").read()
 
+# 获取主目录路径
 dirt = os.environ['HOME']
+
+# 获取今日0点的时间戳
 def getname():
     return int(time.mktime(datetime.date.today().timetuple()))
 
+# 第一次使用的初始化
 if (os.path.isdir(dirt+"/.timespent") == False):
     os.system("mkdir ~/.timespent")
 
 while (1):
+    # 读入数据
+    # 数据格式： [[窗口名称, 使用次数]]
     t = []
     if os.path.isfile(dirt+"/.timespent/{}.csv".format(getname())):
         with open(dirt+"/.timespent/{}.csv".format(getname()), "r") as f:
             if (f.readable()):
                 t = f.read().splitlines()
     t = [(i.split(', ')) for i in t]
+
+    # 获取焦点窗口名称
     string = getfocus()
+    # 若获取到的窗口为桌面(即什么都没有获取到)
     if (len(string) > 2):
         name = string.split('"')[-2]
     else:
         name = "desktop"
+
+    # 若今天已经使用过该程序，则使用次数+1
     flag = False
     for i in t:
         if (i[0] == name):
             i[1] = str(int(i[1]) + 1)
             flag = True
+    
+    # 否则，加入今天使用的该程序
     if (flag == False):
         t.append([name, 1])
+
+    # 保存数据，保存到~/.timespent/当天的时间戳.csv
     with open(dirt+"/.timespent/{}.csv".format(getname()), "w") as f:
         for i in t:
             f.write("{}, {}\n".format(i[0], i[1]))
+    
+    # 保存日志
     log = open(dirt+"/.timespent/log.txt", 'a')
     log.write("{} {}\n".format(time.time(), t))
     log.close()
+
+    # 重复
     time.sleep(10)
